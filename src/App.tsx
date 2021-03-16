d7f2f77b6d7bd13cad406b1188c0b11061fd92bf
Mark Bourke <markkbourke@gmail.com>
2021-03-16 19:03:33 +0000

    Add icon and properly size toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import SidebarSplitViewController from './SidebarSplitViewController'
 import LoginView from "./LoginView";
 import MeetingsOverviewView from "./MeetingsOverviewView";
 import CalendarView from "./CalendarView";
+import calendar from './icons/Calendar.svg'
+
+export const APP_COLOR = "#6a53e4";
 
 function App() {
   if (isSignedIn()) {
     let meetings: any[] = []; // TODO: fetch from API
-    let mainView = <CalendarView meetings={meetings}/>;
-    let detailView = <MeetingsOverviewView meetings={meetings}/>;
-    let icon = ''; // TODO: add icon asset
-    let page = {mainView:mainView, detailView:detailView, icon:icon};
+    const mainView = <CalendarView meetings={meetings}/>;
+    const detailView = <MeetingsOverviewView meetings={meetings}/>;
+    const page = {mainView:mainView, detailView:detailView, icon:calendar};
     return <SidebarSplitViewController  pages={[page]}/>;
   } else {
     return <LoginView />;
--- src/SidebarSplitViewController.tsx
+++ src/SidebarSplitViewController.tsx
@@ -1,6 +1,7 @@
 import './SidebarSplitViewController.css';
 import React from 'react';
 import {Col, Container, Row} from "react-bootstrap";
+import activeIndicator from './icons/Active indicator.svg'
 
 type SidebarSplitViewControllerState = {
     index: number
@@ -26,17 +27,47 @@ export default class SidebarSplitViewController extends React.Component<SidebarS
         }
     }
 
+    currentPage() {
+        return this.props.pages[this.state.index]
+    }
+
+    activityIndicatorForIndex(index: number) {
+        if (this.state.index == index) {
+            return <img className="toolbar-active-indicator" src={activeIndicator}/>;
+        } else {
+            return undefined;
+        }
+    }
+
     sidebar() {
-        return undefined;
+        return (
+            <div className="toolbar">
+                <div>
+
+                </div>
+                <div className="toolbar-items-section">
+                    {
+                        this.props.pages.map((value, index) => {
+                            return (
+                                <div>
+                                    {this.activityIndicatorForIndex(index)}
+                                    <img style={{opacity: this.state.index == index ? "100%" : "40%"}} className="toolbar-item" src={value.icon}/>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        );
     }
 
     render() {
         return (
             <Container fluid>
                 <Row>
-                    <Col xs={1}>{this.sidebar()}</Col>
-                    <Col>{this.props.pages[this.state.index].mainView}</Col>
-                    <Col xs={4}>{this.props.pages[this.state.index].detailView}</Col>
+                    {this.sidebar()}
+                    <Col>{this.currentPage().mainView}</Col>
+                    <Col xs={4}>{this.currentPage().detailView}</Col>
                 </Row>
             </Container>
         )
